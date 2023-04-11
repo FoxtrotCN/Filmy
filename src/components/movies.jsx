c24ed8289494f75d8189cea63b8d8f0f5149bedc
Fernando <fernandostarlin1012@gmail.com>
2023-04-11 12:42:33 -0400

    We've added functionality to our delete button. Now we can delete a movie from the list and that means update the state.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useState } from "react";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
 
 function Movies() {
   const [movies, setMovies] = useState(getMovies());
 
+  const handleDelete = (movie) => {
+    const filteredMovies = movies.filter((m) => m._id !== movie._id);
+    setMovies(filteredMovies);
+  };
+
   return (
     <>
       <h1>Movies Component</h1>
+
       <table className="table">
         <thead>
           <tr>
@@ -15,8 +21,10 @@ function Movies() {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
+
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
@@ -24,6 +32,14 @@ function Movies() {
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
