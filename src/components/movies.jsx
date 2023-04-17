ca2571aa4f516ff1b859e5c5f7c888553e046c79
Fernando <fernandostarlin1012@gmail.com>
2023-04-17 11:41:29 -0400

    Extracting the Movies Table

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react";
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import ListGroupGenres from "./common/listGroupGenres";
 import { useState } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
+import MoviesTable from "./moviesTable";
 
 function Movies() {
   const [allMovies, setAllMovies] = useState([]);
   const [allGenres, setAllGenres] = useState([]);
-  // const [genre, setGenre] = useState({});
   const [selectedGenre, setSelectedGenre] = useState(null);
   const [pageSize, setPageSize] = useState(4);
   const [currentPage, setCurrentPage] = useState(1);
@@ -85,44 +84,12 @@ function Movies() {
           <p className={getBadgeMessage()}>
             Showing {filteredMovies.length} movies in the database.
           </p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
 
-            <tbody>
-              {movies.map((movie) => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <Like
-                      liked={movie.liked}
-                      onClick={() => handleLike(movie)}
-                    />
-                  </td>
-                  <td>
-                    <button
-                      onClick={() => handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
+          <MoviesTable
+            movies={movies}
+            onLike={handleLike}
+            onDelete={handleDelete}
+          />
           <Pagination
             itemsCount={filteredMovies.length}
             pageSize={pageSize}
